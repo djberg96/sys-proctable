a3359d9ad153020a71f26e8a64502921621582b1
Ryan Cunningham <rcunning@gmail.com>
2012-08-08 19:07:31 -0700

    Update lib/linux/sys/proctable.rb
    Added Errno::ENOENT to proc IO.read. We were seeing this exception regularly for processes the simply check for other running processes every 5 seconds.    

--- lib/linux/sys/proctable.rb
+++ lib/linux/sys/proctable.rb
@@ -134,7 +134,7 @@ module Sys
             key, value = str.split('=')
             struct.environ[key] = value
           }
-        rescue Errno::EACCES, Errno::ESRCH
+        rescue Errno::EACCES, Errno::ESRCH, Errno::ENOENT
           # Ignore and move on.
         end
 
