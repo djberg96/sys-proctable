cd4dd4e253bd777bd74c47b0b1d6865f47f808a4
Daniel Berger <djberg96@gmail.com>
2014-03-04 08:01:50 -0700

    Updated require statement.

--- test/test_sys_proctable_linux.rb
+++ test/test_sys_proctable_linux.rb
@@ -4,9 +4,7 @@
 # Test suite for the Linux version of the sys-proctable library. You
 # should run these tests via the 'rake test' task.
 #######################################################################
-require 'rubygems'
-gem 'test-unit'
-
+require 'test-unit'
 require 'sys/proctable'
 require 'test/test_sys_proctable_all'
 include Sys
