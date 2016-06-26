97ed01a353bd05cd84eefb8699d3bce821656fa4
Daniel Berger <dberger@redhat.com>
2016-06-26 14:51:43 -0600

    Replace sysctl with proc lib.

--- lib/darwin/sys/proctable.rb
+++ lib/darwin/sys/proctable.rb
@@ -3,84 +3,91 @@ require 'ffi'
 module Sys
   class ProcTable
     extend FFI::Library
-    ffi_lib FFI::Library::LIBC
+    ffi_lib 'proc'
 
-    attach_function(
-      :sysctl,
-      [:pointer, :uint, :pointer, :pointer, :pointer, :size_t],
-      :int
-    )
+    PROC_ALL_PIDS       = 1
+    PROC_PIDTASKALLINFO = 2
+    PROC_PIDTASKINFO    = 4
 
-    attach_function(:strerror, [:int], :string)
+    MAXCOMLEN = 16
 
-    private_class_method :sysctl
-
-    CTL_KERN      = 1
-    KERN_PROC     = 14
-    KERN_PROC_ALL = 0
-
-    # sizeof(struct kinfo_proc) == 648
-
-    # Process info appears to be stored in a rat hole of nested
-    # structs on OSX. :(
-
-    # extern_proc
-    class KpProcStruct < FFI::Struct
+    class ProcBsdInfo < FFI::Struct
       layout(
-        :p_un, UnUnion,
-        :p_vmspace, VmspaceStruct,
-
+        :pbi_flags, :uint32_t,
+        :pbi_status, :uint32_t,
+        :pbi_xstatus, :uint32_t,
+        :pbi_pid, :uint32_t,
+        :pbi_ppid, :uint32_t,
+        :pbi_uid, :uid_t,
+        :pbi_gid, :uid_t,
+        :pbi_ruid, :uid_t,
+        :pbi_rgid, :gid_t,
+        :pbi_svuid, :uid_t,
+        :pbi_svgid, :gid_t,
+        :rfu_1, :uint32_t,
+        :pbi_comm, [:char, MAXCOMLEN],
+        :pbi_name, [:char, MAXCOMLEN * 2],
+        :pbi_nfiles, :uint32_t,
+        :pbi_pgid, :uint32_t,
+        :pbi_pjobc, :uint32_t,
+        :e_tdev, :uint32_t,
+        :e_tpgid, :uint32_t,
+        :pbi_nice, :int32_t,
+        :pbi_start_tvsec, :uint64_t,
+        :pbi_start_tvusec, :uint64_t
       )
     end
 
-    class EProcStruct < FFI::Struct
+    class ProcTaskInfo < FFI::Struct
       layout(
-        :e_paddr, ProcStruct,
-        :e_sess, SessionStruct,
-        :e_pcred, PCredStruct,
-        :e_ucred, UCredStruct,
-        :e_vm, VMSpaceStruct,
-        :e_ppid, :int,
-        :e_pgid, :int,
-        :e_jobc, :short,
-        :e_tdev, :uint, # Not sure
-        :e_tpgid, :int,
-        :e_tsess, SessionStruct,
-        :e_wmesg, [:char, 8],
-        :e_xsize, :ulong, # Not sure
-        :e_xrssize, :short,
-        :e_flag, :int32,
-        :e_login, [:char, 12],
-        :e_spare, [:int32, 4]
+        :pti_virtual_size, :uint64_t,
+        :pti_resident_size, :uint64_t,
+        :pti_total_user, :uint64_t,
+        :pti_total_system, :uint64_t,
+        :pti_threads_user, :uint64_t,
+        :pti_threads_system, :uint64_t,
+        :pti_policy, :int32_t,
+        :pti_faults, :int32_t,
+        :pti_pageins, :int32_t,
+        :pti_cow_faults, :int32_t,
+        :pti_messages_sent, :int32_t,
+        :pti_messages_received, :int32_t,
+        :pti_syscalls_mach, :int32_t,
+        :pti_syscalls_unix, :int32_t,
+        :pti_csw, :int32_t,
+        :pti_threadnum, :int32_t,
+        :pti_numrunning, :int32_t,
+        :pti_priority, :int32_t
       )
     end
 
-    class KInfoStruct < FFI::Struct
-      layout(
-        :kp_proc, KpProcStruct,
-        :eproc, EProcStruct
-      )
+    class ProcTaskAllInfo < FFI::Struct
+      layout(:pbsd, ProcBsdInfo, :ptinfo, ProcTaskInfo)
     end
 
-    def self.ps
-      len = FFI::MemoryPointer.new(:size_t)
-      mib = FFI::MemoryPointer.new(:int, 4)
+    attach_function :proc_listpids, [:uint32_t, :uint32_t, :pointer, :int], :int
+    attach_function :proc_listallpids, [:pointer, :int], :int
 
-      mib.write_array_of_int([CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0])
+    def self.ps(pid = nil)
+      size = proc_listpids(PROC_PIDTASKALLINFO, 0, nil, 0)
+      ptr  = FFI::MemoryPointer.new(ProcTaskAllInfo, size)
 
-      # First pass, determine length
-      if sysctl(mib, 4, nil, len, nil, 0) < 0
-        raise Error, "sysctl function failed: " + strerror(FFI.errno)
+      if proc_listpids(PROC_PIDTASKALLINFO, 0, ptr, ptr.size) == 0
+        raise SystemCallError.new('proc_listpids', FFI.errno)
       end
 
-      procs = FFI::MemoryPointer.new(:pointer, len.read_long)
-      len   = FFI::MemoryPointer.new(:size_t)
+      count = proc_listallpids(ptr, ptr.size)
 
-      if sysctl(mib, 4, procs, len, nil, 0) < 0
-        raise Error, "sysctl function failed: " + strerror(FFI.errno)
+      raise SystemCallError.new('proc_listallpids', FFI.errno) if count == 0
+
+      # TODO: Fix
+      0.upto(count) do |index|
+        struct = ProcTaskAllInfo.new(ptr + (index * ProcTaskAllInfo.size))
+        p struct[:pbsd][:pbi_pid]
+        p struct[:pbsd][:pbi_comm].to_s
       end
-
-      # count = len.read_long / KInfoStruct.size
     end
   end
 end
+
+Sys::ProcTable.ps
