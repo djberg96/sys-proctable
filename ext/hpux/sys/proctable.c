f52c71a701ee6bfe12f54a77f30fa74a6923c1d8
Daniel Berger <djberg96@gmail.com>
2016-09-18 22:16:23 -0600

    Bump version to 1.1.2.

--- ext/hpux/sys/proctable.c
+++ ext/hpux/sys/proctable.c
@@ -304,8 +304,8 @@ void Init_proctable()
    /* There is no constructor */
    rb_funcall(cProcTable, rb_intern("private_class_method"), 1, ID2SYM(rb_intern("new")));
 
-   /* 1.1.1: The version of the sys-proctable library. */
-   rb_define_const(cProcTable, "VERSION", rb_str_new2("1.1.1"));
+   /* 1.1.2: The version of the sys-proctable library. */
+   rb_define_const(cProcTable, "VERSION", rb_str_new2("1.1.2"));
 
    /* Structs */
 
