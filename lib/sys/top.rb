e9efc53f9f064f4f65aa15200c9e6e4c30712562
Daniel Berger <djberg96@gmail.com>
2017-03-30 12:08:53 -0600

    Make pid the default sort field on Darwin.

--- lib/sys/top.rb
+++ lib/sys/top.rb
@@ -7,21 +7,22 @@ module Sys
   class Top
 
     # The version of the sys-top library
-    VERSION = '1.0.4'
+    VERSION = '1.0.5'.freeze
 
     # Returns an array of Struct::ProcTableStruct elements containing up
     # to +num+ elements, sorted by +field+. The default number of elements
     # is 10, while the default field is 'pctcpu'.
     #
-    # Exception: the default sort field is 'pid' on AIX and Windows.
+    # Exception: the default sort field is 'pid' on AIX, Darwin and Windows.
     #
     def self.top(num=10, field='pctcpu')
       field = field.to_s if field.is_a?(Symbol)
 
       aix = RbConfig::CONFIG['host_os'] =~ /aix/i
+      darwin = RbConfig::CONFIG['host_os'] =~ /darwin/i
 
       # Sort by pid on Windows and AIX by default
-      if (File::ALT_SEPARATOR || aix) && field == 'pctcpu'
+      if (File::ALT_SEPARATOR || aix || darwin) && field == 'pctcpu'
         field = 'pid'
       end
 
