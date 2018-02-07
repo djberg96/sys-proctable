41623b3cb03757ae07c8025a06b4df65aa476795
Nick LaMuro <nicklamuro@gmail.com>
2018-02-07 17:37:00 -0600

    [darwin] Refactor single pid vs all pids for .ps
    This is a refactoring of the `.ps` method to reduce the amount of extra
    allocations necessary and unnecessary looping over unneeded pids when
    `pid` is passed.
    
    Effectively, this splits the method into pieces:
    
    - If the method has a pid passed in, it will call get_info_for_pid, with
      the pid as the argument
    - If the pid value is `nil`, then the get_info_for_all_pids method will
      be called, and get_info_for_pid will be called for each pid that it
      finds
    
    This means that the proc_pidinfo method won't get called when a single
    pid is requested for, saving a few object allocations.
    
    All previous functionality should still be in place.

--- lib/darwin/sys/proctable.rb
+++ lib/darwin/sys/proctable.rb
@@ -172,10 +172,31 @@ module Sys
     #   # Same as above, but do not include thread information
     #   p ProcTable.ps(pid: 1001, thread_info: false)
     #
-    def self.ps(**kwargs)
+    def self.ps(**kwargs, &block)
       pid = kwargs[:pid]
       raise TypeError unless pid.is_a?(Numeric) if pid
+      pid ? get_info_for_pid(pid, kwargs, &block) : get_info_for_all_pids(kwargs, &block)
+    end
 
+    private
+
+    def self.get_info_for_pid(pid, **kwargs)
+      info = get_proc_task_info(pid)
+      return if info.nil?
+
+      struct = ProcTableStruct.new
+
+      # Pass by reference
+      get_cmd_args_and_env(pid, struct)
+      get_thread_info(pid, struct, info[:ptinfo]) unless kwargs[:thread_info] == false
+      apply_info_to_struct(info, struct)
+
+      struct.freeze
+      yield struct if block_given?
+      struct
+    end
+
+    def self.get_info_for_all_pids(**kwargs)
       num = proc_listallpids(nil, 0)
       ptr = FFI::MemoryPointer.new(:pid_t, num)
       num = proc_listallpids(ptr, ptr.size)
@@ -186,19 +207,8 @@ module Sys
       array = block_given? ? nil : []
 
       pids.each do |lpid|
-        next unless pid == lpid if pid
-        info = get_proc_task_info(pid)
-        next if info.nil?
-
-        struct = ProcTableStruct.new
-
-        # Pass by reference
-        get_cmd_args_and_env(lpid, struct)
-        get_thread_info(lpid, struct, info[:ptinfo]) unless kwargs[:thread_info] == false
-        apply_info_to_struct(info, struct)
-
-
-        struct.freeze
+        struct = get_info_for_pid(lpid, kwargs)
+        next if struct.nil?
 
         if block_given?
           yield struct
@@ -208,10 +218,8 @@ module Sys
       end
 
       return nil if array.nil?
-      pid ? array.first : array
+      array
     end
-
-    private
 
     def self.get_proc_task_info(pid)
       raise TypeError unless pid.is_a?(Numeric)
