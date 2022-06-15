463a9e56f1bd283f021f55ad3627a155aeb549c3
Daniel Berger <daniel.berger@covermymeds.com>
2022-06-15 07:05:22 -0400

    Add exclusion filters based on OS tags.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,11 @@ require 'sys-proctable'
 require 'sys-top'
 
 RSpec.configure do |config|
+  config.filter_run_excluding(:aix) unless RbConfig::CONFIG['host_os'] =~ /aix/i
+  config.filter_run_excluding(:darwin) unless RbConfig::CONFIG['host_os'] =~ /mac|darwin/i
+  config.filter_run_excluding(:linux) unless RbConfig::CONFIG['host_os'] =~ /linux/i
+  config.filter_run_excluding(:freebsd) unless RbConfig::CONFIG['host_os'] =~ /freebsd/i
+  config.filter_run_excluding(:sunos) unless RbConfig::CONFIG['host_os'] =~ /sunos|solaris/i
+  config.filter_run_excluding(:windows) unless Gem.win_platform?
   config.filter_run_excluding(:jruby) if RUBY_PLATFORM == 'java'
 end
