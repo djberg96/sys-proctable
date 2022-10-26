7fe2dae6e24fb0b265bd2a0f39cf987fcd32558a
Robert Waffen <rw@betadots.de>
2022-10-26 10:03:28 +0200

    Skip 0 byte pseudo kernel processes
    Fixes: #105

--- lib/sunos/sys/proctable.rb
+++ lib/sunos/sys/proctable.rb
@@ -254,6 +254,11 @@ module Sys
         # Skip over any entries we don't have permissions to read
         next unless File.readable?("/proc/#{file}/psinfo")
 
+        # Skip 0 byte as-files because these are no "real" processes
+        # These are things like sched or initrd
+        # These are some kind of pseudo processes from the kernel itself
+        next if File.size("/proc/#{file}/as") == 0
+
         data = IO.read("/proc/#{file}/psinfo") rescue next
         psinfo = PSInfo.new(FFI::MemoryPointer.from_string(data))
 
