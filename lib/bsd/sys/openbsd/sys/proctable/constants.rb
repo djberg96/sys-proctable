e9178068747fbce382e09b6bb0ed371f6c4cfdd8
Jacob Hoopes <jake.hoopes@gmail.com>
2023-05-18 12:28:47 -0600

    lib/bsd/sys/openbsd/sys/proctable/constants.rb - change ESIZE value

--- lib/bsd/sys/openbsd/sys/proctable/constants.rb
+++ lib/bsd/sys/openbsd/sys/proctable/constants.rb
@@ -8,7 +8,7 @@ module Sys
     KERN_PROC_ALL  = 0
     KERN_PROC_PID = 1
 
-    ESIZE = 4
+    ESIZE = 0
     # KVM_NO_FILES = 0x80000000 # To big for int type but defined in kvm.h
     KVM_NO_FILES = 0
   end
