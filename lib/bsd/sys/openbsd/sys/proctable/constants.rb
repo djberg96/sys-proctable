e6f08bb2e5cb0691c41df9e69990b9de47bd52e6
Jacob Hoopes <jake.hoopes@gmail.com>
2023-05-18 12:23:44 -0600

    lib/bsd/sys/openbsd/sys/proctable/constants.rb - implement FFI::ConstGenerator for KVM_NO_FILES from kvm.h #tweak

--- lib/bsd/sys/openbsd/sys/proctable/constants.rb
+++ lib/bsd/sys/openbsd/sys/proctable/constants.rb
@@ -9,6 +9,7 @@ module Sys
     KERN_PROC_PID = 1
 
     ESIZE = 4
-    KVM_NO_FILES = 2147483648
+    # KVM_NO_FILES = 0x80000000 # To big for int type but defined in kvm.h
+    KVM_NO_FILES = 0
   end
 end
