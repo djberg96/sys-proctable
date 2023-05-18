c9ad616e6d6dc72c8c4b7a49efcb85f152005d40
Jacob Hoopes <jake.hoopes@gmail.com>
2023-05-18 10:56:21 -0600

    lib/bsd/sys/openbsd/sys/proctable.rb - attach_variable KVM_NO_FILES from kvm.h #bugfix

--- lib/bsd/sys/openbsd/sys/proctable/functions.rb
+++ lib/bsd/sys/openbsd/sys/proctable/functions.rb
@@ -10,6 +10,6 @@ module Sys
     attach_function :kvm_close, [:pointer], :int
     attach_function :kvm_getprocs, [:pointer, :int, :int, :size_t, :pointer], :pointer
     attach_function :kvm_getargv, [:pointer, :pointer, :int], :pointer
-    attach_variable :kvm_no_files, :int
+    attach_variable :KVM_NO_FILES, :int
   end
 end
